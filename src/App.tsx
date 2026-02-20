73a8f4c6afaa793705cdb19a04c23fcc0dc88b6d
hana-zouabi <hanazouabi72@gmail.com>
2026-02-20 12:21:48 +0100

    Commit initial : projet React TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Greeting from "./components/Greeting";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Hana" />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
